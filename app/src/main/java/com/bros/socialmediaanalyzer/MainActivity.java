413b6abe74d1f8847500ee417fdbf9574461ede9
Batuhan <batuhanmertkarabulut@gmail.com>
2017-11-25 11:44:41 +0300

    First

--- app/src/main/java/com/bros/socialmediaanalyzer/MainActivity.java
+++ app/src/main/java/com/bros/socialmediaanalyzer/MainActivity.java
@@ -2,6 +2,7 @@ package com.bros.socialmediaanalyzer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton b1;
     }
 }
